01d2dc709b1a259dcddd7cb937708f3edcf64dfa
VIVIAN RION <vivianrionmarandi@gmail.com>
2024-03-16 12:35:07 +0600

    header component uploaded for desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Header from './components/Site Main Header/header'
 
 function App() {
 
   return (
 
-    <div className='font-sans'>
+    <div className='font-lexend mx-auto container'>
 
-      
+      <Header></Header>
+
+
 
 
     </div>
---
+++ src/components/Site Main Header/header.jsx
@@ -0,0 +1,57 @@
+const Header = () => {
+
+    return (
+
+        <div className='my-12'>
+
+            <div className="navbar">
+
+                <div className="navbar-start">
+
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-base font-normal text-neutral-500">
+                            <li><a>Home</a></li>
+                            <li><a>Recipes</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Search</a></li>
+                        </ul>
+                    </div>
+
+                    <a className="btn btn-ghost text-3xl font-bold text-slate-950">Recipe Calories</a>
+
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-base font-normal text-neutral-500">
+                        <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+
+                <div className="navbar-end">
+
+                    <label className="input input-bordered hidden lg:flex items-center gap-2">
+                        <input type="text" className="grow" placeholder="Search" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    </label>
+
+                    <div className='w-12 h-12 flex justify-center items-center lg:ml-4'>
+                        <a className="btn  bg-lime-500">
+                            <img className='w-6 h-6' src="../../../images/navbarIcon.png" alt="...Loading" /></a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+
+};
+
+export default Header;
