fb52b09f9d23797151da232762c6f835105562ef
VIVIAN RION <vivianrionmarandi@gmail.com>
2024-03-17 17:28:03 +0600

    want to cook and currently cooking section uploaded

--- src/components/Recipes/recipesSection.jsx
+++ src/components/Recipes/recipesSection.jsx
@@ -4,19 +4,19 @@ import SingleItemRecipes from "./singleItemRecipes";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-
 const RecipesSection = () => {
 
     const [recipes, setRecipes] = useState([]);
+    const [wantToCook, setWantToCook] = useState([]);
+    const [currentlyCook, setCurrentlyCook] = useState([]);
+    const [time, setTime] = useState(0);
+    const [calories, setcalories] = useState(0);
 
     useEffect(() => {
-        fetch('../../../public/fakeJsonData.json')
+        fetch('/public/fakeJsonData.json')
             .then(response => response.json())
             .then(data => setRecipes(data))
     }, []);
-
-    const [wantToCook, setWantToCook] = useState([]);
 
     const handleWantoCook = recipe => {
         const isExist = wantToCook.find(item => item.recipe_id === recipe.recipe_id);
@@ -24,6 +24,24 @@ const RecipesSection = () => {
             setWantToCook([...wantToCook, recipe]);
         } else {
             toast('Already Added. Please Try Another One. Thank You!!!')
+        }
+    }
+
+    const removeData = id => {
+        const remainingData = wantToCook.filter(item => id !== item.recipe_id);
+        setWantToCook(remainingData);
+    }
+
+    const currentCookingData = (items, addtime, cal) => {
+        const isCooking = currentlyCook.find(element => element.recipe_id === items.recipe_id);
+        if (isCooking) {
+            toast(' Still Cooking. Wait Please!!!');
+            setTime(time);
+            setcalories(calories);
+        } else {
+            setCurrentlyCook([...currentlyCook, items]);
+            setTime(time + addtime);
+            setcalories(calories + cal);
         }
     }
 
@@ -46,7 +64,7 @@ const RecipesSection = () => {
 
                 </div>
 
-                <div className="w-2/5 h-auto py-8 border border-neutral-300 rounded-3xl">
+                <div className="w-2/5 h-auto px-5 py-8 border border-neutral-300 rounded-3xl">
 
                     <div className="min-h-96">
 
@@ -76,7 +94,10 @@ const RecipesSection = () => {
                                             <td>{item.recipe_name}</td>
                                             <td>{item.preparation_time_min}</td>
                                             <td>{item.calories}</td>
-                                            <td><button className="bg-green-500 text-slate-950 px-4 py-3 font-semibold rounded-full">Preparing</button></td>
+                                            <td><button onClick={() => {
+                                                removeData(item.recipe_id)
+                                                currentCookingData(item, item.preparation_time_min, item.calories)
+                                            }} className="bg-green-500 text-slate-950 px-4 py-3 font-semibold rounded-full">Preparing</button></td>
                                         </tr>
 
                                     ))
@@ -88,9 +109,9 @@ const RecipesSection = () => {
 
                     </div>
 
-                    <div className="min-h-96 mt-20">
+                    <div className="min-h-52 my-20">
 
-                        <h4 className="text-center pb-4 text-2xl font-semibold mx-16 border-b border-neutral-300">Want to cook: {wantToCook.length}</h4>
+                        <h4 className="text-center pb-4 text-2xl font-semibold mx-16 border-b border-neutral-300">Currently cooking: {currentlyCook.length}</h4>
 
                         <table className="table mx-auto font-fira-sans text-base font-medium">
 
@@ -108,9 +129,39 @@ const RecipesSection = () => {
 
                             <tbody className="text-neutral-400 font-normal">
 
+                                {
+                                    currentlyCook.map((item, index) => (
+                                        <tr>
+                                            <th>{index + 1}</th>
+                                            <td>{item.recipe_name}</td>
+                                            <td>{item.preparation_time_min}</td>
+                                            <td>{item.calories}</td>
+                                        </tr>
+                                    )
+                                    )
+                                }
+
                             </tbody>
 
                         </table>
+
+                    </div>
+
+                    <div className="flex justify-end mr-10">
+
+                        <div className="text-base text-start font-medium text-sky-950 leading-7 flex gap-x-5 items-center">
+
+                            <div>
+                                <h6>Total Time = </h6>
+                                <h6>{time}</h6>
+                            </div>
+
+                            <div>
+                                <h6>Total Calories =</h6>
+                                <h6>{calories}</h6>
+                            </div>
+
+                        </div>
 
                     </div>
 
